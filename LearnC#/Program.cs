1dd0fa8c52ec03c18bc84d01c8f2a03730ff802c
NikkiShuRA <xak-bag-303@yandex.ru>
2025-05-22 15:06:53 +0500

    Добавьте файлы проекта.

---
+++ LearnC#/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LearnCS;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("1 = s");
+
+
+        bool close = true;
+        while(close)
+        {
+            var key = Console.ReadKey(true).KeyChar;
+
+            switch (key)
+            {
+                case '1':
+                    Console.WriteLine("s");
+                    break;
+
+                case '0':
+                    close = false;
+                    break;
+            }
+        }
+    }
+}
+
+class MyList
+{
+
+    static List<string> myList = new List<string>();
+
+    public static void AddNewValue(string value)
+    {
+        myList.Add(value);
+    }
+
+    public static void CheckList()
+    {
+        foreach(string value in myList)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+}
